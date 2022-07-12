6e186d0ec869198778a5d93dcfa404964984c88a
Casey Hawkins <hawkcase@oregonstate.edu>
2022-07-12 00:26:27 -0500

    V1.0
    Initial implementation of project

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ redirect_uri = 'http://127.0.0.1:8080/oauth'
 scope = ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile', 'openid']
 oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
 
+
 @app.route('/')
 def index():
 	authorization_url, state = oauth.authorization_url('https://accounts.google.com/o/oauth2/auth', access_type="offline", prompt="select_account")
-	#return render_template('oauth.html')
 	return render_template('index.html', authorization_url=authorization_url)
 
 @app.route('/oauth')
@@ -44,66 +44,18 @@ def oauthroute():
 	token = oauth.fetch_token('https://accounts.google.com/o/oauth2/token', authorization_response=request.url, client_secret=client_secret)
 	id_info = id_token.verify_oauth2_token(token['id_token'], requests.Request(), client_id)
 	# clock_skew_in_seconds=30
-	query = client.query(kind="users")
+	query = client.query(kind="user")
 	query.add_filter("sub", "=", id_info['sub'])
 	result = list(query.fetch())
 
 	if len(result) == 0:
-		new_user = datastore.entity.Entity(key=client.key('users'))
+		new_user = datastore.entity.Entity(key=client.key('user'))
 		new_user.update({'email': id_info['email'], 'sub': id_info['sub']})
 		client.put(new_user)
 		return render_template('oauth.html', token=token['id_token'], sub=id_info['sub'], email=id_info['email'])
 	elif len(result) == 1:
 		return render_template('oauth.html', token=token['id_token'], sub=id_info['sub'], email=id_info['email'])
 
-@app.route('/users', methods=['GET'])
-def get_users():
-	if request.method == 'GET':
-		query = client.query(kind="users")
-		results = list(query.fetch())
-
-		for entity in results:
-			entity["id"] = entity.key.id
-			entity["self"] = request.url + "/" + str(entity.key.id)
-
-		return (jsonify(results), 200)
-	else:
-		return "Method not allowed", 405
-
-@app.route('/users/<uid>', methods=['GET'])
-def get_user_id(uid):
-	if request.method == 'GET':
-		
-		token = request.headers.get('Authorization')
-		
-		if token:
-			token = token.split(" ")[1]
-
-			try:
-				id_info = id_token.verify_oauth2_token(token, requests.Request(), client_id)
-				sub = id_info['sub']
-			except:
-				return (jsonify({"Error": "Invalid JWT"}), 401)
-		else:
-			return (jsonify({"Error": "JWT not found"}), 401)
-		
-		if sub != uid:
-			return(jsonify({'Error': 'You do not have access to this user'}), 401)
-			
-		query = client.query(kind="users")
-		query.add_filter("sub", "=", uid)
-		results = list(query.fetch())
-
-		if len(results) == 0:
-			return(jsonify({'Error': 'User does not exist'}), 401)
-
-		for entity in results:
-			entity["id"] = entity.key.id
-			entity["self"] = request.url
-
-		return (jsonify(results), 200)
-	else:
-		return "Method not allowed", 405
 
 @app.route('/schedules', methods=['POST','GET', "PUT", "PATCH"])
 def schedules_get_post():
@@ -111,7 +63,7 @@ def schedules_get_post():
 	
 		content = request.form
 		
-		if "Date" in content and "Shift" in content:
+		if content["Date"] != '' and content["Shift"] != '':
 			
 			new_schedule = datastore.entity.Entity(key=client.key("schedule"))
 			new_schedule.update({"Date": content["Date"],"Shift": content["Shift"]})
@@ -120,37 +72,30 @@ def schedules_get_post():
 			if (content["Date"] + content["Shift"]) in date_shifts:
 				flash("Schedule is already present", category='error')
 				query = client.query(kind="schedule")
+				query.order = ["Date"]
 				results = list(query.fetch())
-				return render_template('full_schedule.html', data=results, email=id_info['email'], token=token)
+				return render_template('full_schedule.html', data=results)
 			client.put(new_schedule)
 			new_schedule['id'] = new_schedule.key.id
 			new_schedule['self'] = request.url + '/' + str(new_schedule.key.id)
 
 			query = client.query(kind="schedule")
+			query.order = ["Date"]
 			results = list(query.fetch())
 			
 			return render_template('full_schedule.html', data=results)
 		else:
-			return (jsonify({"Error": "The request object is missing at least one of the required attributes"}), 400)
+			flash("Schedule is missing a required attribute", category='error')
+			query = client.query(kind="schedule")
+			results = list(query.fetch())
+			return render_template('full_schedule.html', data=results)
 	elif request.method == 'GET':
 
-		token = request.headers.get('Authorization')
-		
-		if token:
-			token = token.split(" ")[1]
-
-			try:
-				id_info = id_token.verify_oauth2_token(token, requests.Request(), client_id)
-				sub = id_info['sub']
-			except:
-				return (jsonify({"Error": "Invalid JWT"}), 401)
-		else:
-			return (jsonify({"Error": "JWT not found"}), 401)
-
 		query = client.query(kind="schedule")
+		query.order = ["Date"]
 		results = list(query.fetch())
 
-		return render_template('full_schedule.html', data=results, email=id_info['email'], token=token)
+		return render_template('full_schedule.html', data=results)
 
 	else:
 		return "Method not allowed", 405
@@ -173,6 +118,82 @@ def delete_schedule():
 
 	else:
 		return "Method not allowed", 405
+
+@app.route('/profiles', methods=['POST','GET', "PUT", "PATCH"])
+def profiles():
+	if request.method == 'POST':
+	
+		content = request.form
+		
+		if content["fName"] != '' and content["lName"] != '' and content["email"] != '' and content["phone"] != '':
+			
+			new_profile = datastore.entity.Entity(key=client.key("profile"))
+			new_profile.update({"fName": content["fName"],"lName": content["lName"], "email": content["email"], "phone": content["phone"]})
+			query = client.query(kind='profile')
+			fName_lName_email_phone = [entity["fName"] + entity["lName"] + entity["email"] + entity["phone"] for entity in query.fetch()]
+			if (content["fName"] + content["lName"] + content["email"] + content["phone"]) in fName_lName_email_phone:
+				flash("User already has a profile", category='error')
+				query = client.query(kind="profile")
+				results = list(query.fetch())
+				return render_template('profiles.html', data=results)
+			client.put(new_profile)
+			new_profile['id'] = new_profile.key.id
+			new_profile['self'] = request.url + '/' + str(new_profile.key.id)
+
+			query = client.query(kind="profile")
+			results = list(query.fetch())
+			
+			return render_template('profiles.html', data=results)
+		else:
+			flash("Must input all categories", category='error')
+			query = client.query(kind="profile")
+			results = list(query.fetch())
+			return render_template('profiles.html', data=results)
+	elif request.method == 'GET':
+
+		query = client.query(kind="profile")
+		results = list(query.fetch())
+
+		return render_template('profiles.html', data=results)
+
+	else:
+		return "Method not allowed", 405
+
+@app.route('/profiles/<pid>', methods=['GET', 'POST'])
+def get_profile_id(pid):
+	if request.method == 'GET':
+			
+		query1 = client.query(kind="availabilities")
+		query1.add_filter("pid", "=", pid)
+		avails = list(query1.fetch())
+
+		query2 = client.query(kind="profile")		
+		query2.add_filter("email", "=", "hawkinscjh@gmail.com")
+		results = list(query2.fetch())
+		print(results)
+
+		return render_template('user_profile.html', avails=avails, data=results)
+	else:
+		return "Method not allowed", 405
+
+@app.route('/profiles/delete-profile', methods=['POST'])
+def delete_profile():
+	if request.method == 'POST':
+		jsonData = json.loads(request.data)
+		profile_id = jsonData['profile_id']
+		profile_key = client.key("profile", int(profile_id))
+		profile = client.get(key=profile_key)
+		if profile == None:
+			return (jsonify({'Error': 'No profile with this profile_id exists'}), 404)
+		client.delete(profile_key)
+
+		query = client.query(kind="profile")
+		results = list(query.fetch())
+
+		return render_template('profiles.html', data=results)
+
+	else:
+		return "Method not allowed", 405		
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
--- static/script.js
+++ static/script.js
@@ -1,8 +1,25 @@
 function deleteSchedule(schedule_id) {
-  fetch("/schedules/delete-schedule", {
-    method: "POST",
-    body: JSON.stringify({ schedule_id: schedule_id }),
-  }).then((_res) => {
+  if (confirm("Delete schedule?") == true) {
+    fetch("/schedules/delete-schedule", {
+      method: "POST",
+      body: JSON.stringify({ schedule_id: schedule_id }),
+    }).then((_res) => {
+      window.location.href = "/schedules";
+    });
+  } else {
     window.location.href = "/schedules";
-  });
-}
+  }
+};
+
+function deleteProfile(profile_id) {
+  if (confirm("Delete user profile?") == true) {
+    fetch("/profiles/delete-profile", {
+      method: "POST",
+      body: JSON.stringify({ profile_id: profile_id }),
+    }).then((_res) => {
+      window.location.href = "/profiles";
+    });
+  } else {
+    window.location.href = "/profiles";
+  }
+};
