34f1acf2d293d993f5a680d0b616637ff8e6954b
Casey Hawkins <hawkcase@oregonstate.edu>
2022-07-11 20:04:23 -0500

    Added base.html template

--- main.py
+++ main.py
@@ -164,13 +164,4 @@ def delete_schedule():
 		return "Method not allowed", 405
 
 if __name__ == "__main__":
-    # Quick test configuration. Please use proper Flask configuration options
-    # in production settings, and use a separate file or environment variables
-    # to manage the secret key!
-    #app.secret_key = 'super secret key'
-    #app.config['SESSION_TYPE'] = 'filesystem'
-
-    #session.init_app(app)
-
-    #app.debug = True
     app.run(host='127.0.0.1', port=8080, debug=True)
